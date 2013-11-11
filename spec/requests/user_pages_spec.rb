7ed69d3fd7774ac22edf2ecbb07f5dfbb10c9cba
infinitelyfinite <my.email@email.com>
2013-11-11 17:29:25 -0600

    Complete Chapter 8

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -47,11 +47,12 @@ describe "User pages" do
           end
           
           describe "after saving the user" do
-            before { click_button submit }
-            let(:user) { User.find_by(email: 'user@example.com') }
-
-            it { should have_title(user.name) }
-            it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+              before { click_button submit }
+              let(:user) { User.find_by(email: 'user@example.com') }
+              
+              it { should have_link('Sign out') }
+              it { should have_title(user.name) }
+              it { should have_selector('div.alert.alert-success', text: 'Welcome')}
           end
 
           it "should create a user" do
